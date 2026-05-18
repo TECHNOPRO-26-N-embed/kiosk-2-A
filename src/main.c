453b9f42b74b31fd555ac642b08080449fc15f45
Yuki-Yamashita-TP <282408008+Yuki-Yamashita-TP@users.noreply.github.com>
2026-05-18 15:05:09 +0900

    Revert "レシート機能の追加"
    This reverts commit 4ed3ecb1e0670ab59ac404c1c92e73549875ff77.

--- src/main.c
+++ src/main.c
@@ -11,11 +11,6 @@ int main(void) {
         printf("Enter your choice: ");
         scanf("%d", &choice);
         if (choice == 1) {
-            printf("お買い上げありがとうございます！\n");
-            printf("*********************************\n");
-            printf("購入した商品\n");
-            printf("ぼんじり    200円\n");
-            printf("*********************************\n");
             break;
         } else if (choice == 2) {
             return 0;
